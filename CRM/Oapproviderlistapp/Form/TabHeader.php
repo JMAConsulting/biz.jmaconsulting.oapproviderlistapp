b0d76d465addfdbacdd687b6ad5424357524c462
Monish Deb <deb.monish@gmail.com>
2019-04-02 14:52:20 +0530

    add missing css

--- CRM/Oapproviderlistapp/Form/TabHeader.php
+++ CRM/Oapproviderlistapp/Form/TabHeader.php
@@ -14,6 +14,8 @@ class CRM_Oapproviderlistapp_Form_TabHeader {
       $form->set('tabHeader', $tabs);
     }
     $form->assign_by_ref('tabHeader', $tabs);
+    CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.shoreditch', 'css/custom-civicrm.css',1, 'html-header');
+    CRM_Core_Resources::singleton()->addStyleFile('biz.jmaconsulting.oapproviderlistapp', 'templates/css/oapp.css');
     CRM_Core_Resources::singleton()
       ->addScriptFile('civicrm', 'templates/CRM/common/TabHeader.js', 1, 'html-header')
       ->addSetting(array(
