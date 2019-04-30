c832802ef067bb39400dbc47a44e18fe2d67b5cd
Edsel <edsel.lopez@jmaconsulting.biz>
2019-04-30 13:32:36 +0200

    More functionality for edit listing

--- CRM/Oapproviderlistapp/Form/EditListing.php
+++ CRM/Oapproviderlistapp/Form/EditListing.php
@@ -17,18 +17,53 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
       $result = civicrm_api3('CustomField', 'get', [
         'sequential' => 1,
         'custom_group_id' => $group,
+        'options' => ['sort' => "weight"],
       ])['values'];
       foreach ($result as $field => $value) {
-        $name = sprintf("%s_%d", "custom_" . $value['id']);
-        $form->add(strtolower($value['html_type']), $name, E::ts("%1", [1 => $value['label']]), NULL);
+        $name = sprintf("%s_%d", "custom_", $value['id']);
+        if (strtolower($value['html_type']) == 'multi-select') {
+          $this->addEntityRef('language', E::ts($value['label']), [
+            'entity' => 'OptionValue',
+            'placeholder' => E::ts('- any -'),
+            'multiple' => 1,
+            'api' => [
+              'params' => [
+                'check_permissions' => FALSE,
+                'option_group_id' => $value['option_group_id'],
+              ],
+            ],
+          ]);
+        }
+        if (strtolower($value['html_type']) == 'checkbox') {
+          $options = CRM_Core_OptionGroup::values(CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionGroup', $value['option_group_id'], 'name', 'id'));
+          $this->addCheckBox($name, E::ts("%1", [1 => $value['label']]), $options, NULL, NULL, NULL, NULL, ' &nbsp; ');
+        }
+        elseif (strtolower($value['html_type']) == 'radio') {
+          $this->addYesno($name, E::ts("%1", [1 => $value['label']]), NULL);
+        }
+	elseif (strtolower($value['html_type']) != 'multi-select') {
+          $this->add(strtolower($value['html_type']), $name, E::ts("%1", [1 => $value['label']]), NULL);
+        }
       }
     }
     $this->assign('elementNames', $this->getRenderableElementNames());
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => E::ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+      array(
+        'type' => 'cancel',
+        'name' => E::ts('Cancel'),
+      ),
+    ));
     parent::buildQuickForm();
   }
 
   function postProcess() {
     $values = $this->exportValues();
+    CRM_Core_Error::debug('ga', $values);exit;
     parent::postProcess();
   }
 
