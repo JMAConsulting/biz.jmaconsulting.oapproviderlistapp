90a08f194263ac2eea5c471e08b3d5cdb7adf03a
Edsel <edsel.lopez@jmaconsulting.biz>
2019-04-30 18:06:31 +0530

    More changes

--- CRM/Oapproviderlistapp/Form/EditListing.php
+++ CRM/Oapproviderlistapp/Form/EditListing.php
@@ -9,6 +9,40 @@ use CRM_Oapproviderlistapp_ExtensionUtil as E;
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
 class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
+
+  public $_contactId;
+
+  function preProcess() {
+    $this->_contactId = CRM_Core_Session::getLoggedInContactID();
+    if (empty($this->_contactId)) {
+      CRM_Core_Error::fatal(E::ts('You must be logged in to view this form.'));
+    }
+  }
+
+  function setDefaultValues() {
+    $result = civicrm_api3('CustomField', 'get', [
+      'sequential' => 1,
+      'custom_group_id' => "Contact_general",
+      'return' => 'id',
+    ])['values'];
+    foreach ($result as $fid) {
+      $fieldIds[] = $fid['id'];
+    }
+    $customValues = CRM_Core_BAO_CustomValueTable::getEntityValues($this->_contactId, 'Contact', $fieldIds);
+    foreach ($customValues as $key => $value) {
+      $customValues['custom_' . $key] = $value;
+      if ($key == 67) {
+        $value = array_filter(explode(CRM_Core_DAO::VALUE_SEPARATOR, $value));
+        foreach ($value as $val) {
+          $vals[$val] = 1;
+        }
+        $customValues['custom_' . $key] = $vals;
+      }
+      unset($customValues[$key]);
+    }
+    return $customValues;
+  }
+
   function buildQuickForm() {
     $customGroups = [
       "Contact_general",
@@ -20,9 +54,9 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
         'options' => ['sort' => "weight"],
       ])['values'];
       foreach ($result as $field => $value) {
-        $name = sprintf("%s_%d", "custom_", $value['id']);
+        $name = sprintf("%s_%d", "custom", $value['id']);
         if (strtolower($value['html_type']) == 'multi-select') {
-          $this->addEntityRef('language', E::ts($value['label']), [
+          $this->addEntityRef($name, E::ts($value['label']), [
             'entity' => 'OptionValue',
             'placeholder' => E::ts('- any -'),
             'multiple' => 1,
@@ -34,14 +68,14 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
             ],
           ]);
         }
-        if (strtolower($value['html_type']) == 'checkbox') {
+        elseif (strtolower($value['html_type']) == 'checkbox') {
           $options = CRM_Core_OptionGroup::values(CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionGroup', $value['option_group_id'], 'name', 'id'));
           $this->addCheckBox($name, E::ts("%1", [1 => $value['label']]), $options, NULL, NULL, NULL, NULL, ' &nbsp; ');
         }
         elseif (strtolower($value['html_type']) == 'radio') {
           $this->addYesno($name, E::ts("%1", [1 => $value['label']]), NULL);
         }
-	elseif (strtolower($value['html_type']) != 'multi-select') {
+        else {
           $this->add(strtolower($value['html_type']), $name, E::ts("%1", [1 => $value['label']]), NULL);
         }
       }
@@ -63,7 +97,19 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
 
   function postProcess() {
     $values = $this->exportValues();
-    CRM_Core_Error::debug('ga', $values);exit;
+    foreach ($values as $field => $value) {
+      if (!is_array($value) && strpos($value, ',') !== false) {
+        $value = explode(',', $value);
+      }
+      if (strpos($field, 'custom_') !== false) {
+        $customValues[$field] = $value;
+      }
+    }
+    CRM_Core_BAO_CustomValueTable::postProcess($customValues,
+      'civicrm_contact',
+      $this->_contactId,
+      'Individual'
+    );
     parent::postProcess();
   }
 
