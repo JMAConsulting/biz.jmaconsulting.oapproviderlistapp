29df896d5dfd4577488c401037719c1a8f77dc04
Edsel <edsel.lopez@jmaconsulting.biz>
2019-05-02 16:29:17 +0530

    Added employers

--- CRM/Oapproviderlistapp/Form/EditListing.php
+++ CRM/Oapproviderlistapp/Form/EditListing.php
@@ -23,6 +23,10 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
     if (empty($this->_contactId)) {
       CRM_Core_Error::fatal(E::ts('You must be logged in to view this form.'));
     }
+    $this->_action = CRM_Utils_Request::retrieve('action', 'Positive', $this);
+    if (empty($this->_action)) {
+      $this->_action = CRM_Core_Action::UPDATE;
+    }
   }
 
   function setDefaultValues() {
@@ -89,6 +93,7 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
     $this->add('file', 'image_URL', E::ts('Image'));
     $this->addUploadElement('image_URL');
     $this->assign('elementNames', $this->getRenderableElementNames());
+    $this->assign('employers', $this->getEmployers($this->_contactId));
     $this->addButtons(array(
       array(
         'type' => 'upload',
@@ -100,6 +105,11 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
         'name' => E::ts('Cancel'),
       ),
     ));
+    if ($this->_action & CRM_Core_Action::VIEW) {
+      foreach ($this->getRenderableElementNames() as $element) {
+        $this->freeze($element);
+      }
+    }
     parent::buildQuickForm();
   }
 
@@ -126,7 +136,26 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Core_Form {
       ]);
     }
     parent::postProcess();
+    CRM_Core_Session::singleton()->pushUserContext(CRM_Utils_System::url("civicrm/editlisting",
+      "reset=1&action=4&cid=" . $this->_contactId
+    ));
     CRM_Core_Session::setStatus(E::ts('Your provider listing has been updated.'), ts('Listing Updated'), 'success');
+  }
+
+  function getEmployers($cid) {
+    $sql = "SELECT o.id, o.organization_name, a.street_address, a.city, a.postal_code, e.email, p.phone, sp.abbreviation FROM civicrm_contact o
+      INNER JOIN civicrm_relationship r ON r.contact_id_b = o.id
+      LEFT JOIN civicrm_address a ON a.contact_id = o.id AND a.location_type_id = 2
+      LEFT JOIN civicrm_state_province sp ON sp.id = a.state_province_id
+      LEFT JOIN civicrm_email e ON e.contact_id = %1 AND e.location_type_id = 2
+      LEFT JOIN civicrm_phone p ON p.contact_id = %1 AND p.location_type_id = 2
+      WHERE r.contact_id_a = %1 AND r.relationship_type_id = %2
+      GROUP BY o.id";
+    $rtype = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_RelationshipType', 'Employee of', 'id', 'name_a_b');
+    $employers = CRM_Core_DAO::executeQuery($sql, [1 => [$cid, 'Integer'], 2 => [$rtype, 'Integer']])->fetchAll();
+    if (!empty($employers)) {
+      return $employers;
+    }
   }
 
   /**
--- templates/CRM/Oapproviderlistapp/Form/EditListing.tpl
+++ templates/CRM/Oapproviderlistapp/Form/EditListing.tpl
@@ -1,3 +1,4 @@
+{crmScope extensionKey='biz.jmaconsulting.oapproviderlistapp'}
 {foreach from=$elementNames item=elementName}
   <div class="crm-section">
     <div class="label">{$form.$elementName.label}
@@ -9,6 +10,29 @@
     <div class="clear"></div>
   </div>
 {/foreach}
+<div class="crm-section">
+<div class="label"><label for="{ts}Employer(s){/ts}">{ts}Employer(s){/ts}</label></div>
+<div class="content">
+<table class="form-item">
+{foreach from=$employers item=employer}
+      <tr>
+        <td>
+          {$employer.organization_name} <br/>
+          {$employer.street_address} <br/>
+          {$employer.city}{if $employer.abbreviation}, {$employer.abbreviation}{/if} {$employer.postal_code}
+          <div class=clear></div>
+        </td>
+        <td>
+          {if $employer.phone}Phone: {$employer.phone} <br/>{/if}
+          <a href='mailto:{$employer.email}'>{$employer.email}</a>
+          <div class=clear></div>
+        </td>
+      </tr>
+{/foreach}
+</table>
+</div>
+</div>
 <div class="crm-submit-buttons">
 {include file="CRM/common/formButtons.tpl" location="bottom"}
 </div>
+{/crmScope}
