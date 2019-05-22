4c5d4141de19b7dc8273bffd70523244df5843d6
Monish Deb <deb.monish@gmail.com>
2019-05-22 15:56:32 +0530

    minor fix

--- CRM/Oapproviderlistapp/Form/EditListing.php
+++ CRM/Oapproviderlistapp/Form/EditListing.php
@@ -173,8 +173,8 @@ class CRM_Oapproviderlistapp_Form_EditListing extends CRM_Oapproviderlistapp_For
       INNER JOIN civicrm_relationship r ON r.contact_id_b = o.id
       LEFT JOIN civicrm_address a ON a.contact_id = o.id AND a.location_type_id = 2
       LEFT JOIN civicrm_state_province sp ON sp.id = a.state_province_id
-      LEFT JOIN civicrm_email e ON e.contact_id = %1 AND e.location_type_id = 2
-      LEFT JOIN civicrm_phone p ON p.contact_id = %1 AND p.location_type_id = 2
+      LEFT JOIN civicrm_email e ON e.contact_id = o.id AND e.location_type_id = 2
+      LEFT JOIN civicrm_phone p ON p.contact_id = o.id AND p.location_type_id = 2
       WHERE r.contact_id_a = %1 AND r.relationship_type_id = %2
       GROUP BY o.id";
     $rtype = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_RelationshipType', 'Employee of', 'id', 'name_a_b');
